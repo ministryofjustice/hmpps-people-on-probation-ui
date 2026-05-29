09ee3ade2388d2f1c430953eff310d600baad21e
Elavarasan Pandiyan <elavarasan.pandiyan@justice.gov.uk>
2026-05-29 09:03:21 +0100

    fix integration test hanging

--- integration_tests/specs/ping.spec.ts
+++ integration_tests/specs/ping.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test'
 
-test('ping returns UP', async ({ page }) => {
-  const response = await page.request.get('/ping')
+test('ping returns UP', async ({ request }) => {
+  const response = await request.get('/ping')
   const payload = await response.json()
   expect(payload.status).toBe('UP')
 })
